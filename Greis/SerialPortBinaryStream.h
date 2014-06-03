525f9af091c2183045be0d24c00511d976c4d8e6
sburg <sburg@ifz.ru>
2014-06-03 22:17:19 +0000

    fixed windows build

--- Greis/SerialPortBinaryStream.h
+++ Greis/SerialPortBinaryStream.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <QtCore/QtCore>
 
+#ifdef LINUX
 #include <termio.h>
 #include <fcntl.h>
 #include <err.h>
@@ -11,16 +12,17 @@
 
 static int rate_to_constant(int baudrate) {
 #define B(x) case x: return B##x
-	switch(baudrate) {
-		B(50);     B(75);     B(110);    B(134);    B(150);
-		B(200);    B(300);    B(600);    B(1200);   B(1800);
-		B(2400);   B(4800);   B(9600);   B(19200);  B(38400);
-		B(57600);  B(115200); B(230400); B(460800); B(500000); 
-		B(576000); B(921600); B(1000000);B(1152000);B(1500000); 
-	default: return 0;
-	}
+    switch(baudrate) {
+        B(50);     B(75);     B(110);    B(134);    B(150);
+        B(200);    B(300);    B(600);    B(1200);   B(1800);
+        B(2400);   B(4800);   B(9600);   B(19200);  B(38400);
+        B(57600);  B(115200); B(230400); B(460800); B(500000); 
+        B(576000); B(921600); B(1000000);B(1152000);B(1500000); 
+    default: return 0;
+    }
 #undef B
 }
+#endif
 
 #include "IBinaryStream.h"
 #include "ProjectBase/Exception.h"
@@ -46,32 +48,28 @@ namespace Greis
          */
         SerialPortBinaryStream(std::string portName, unsigned int baudRate) : _io(), _serial(_io, portName)
         {
+#ifdef LINUX
             auto fd = _serial.native();
             struct serial_struct serinfo;
             struct termios options;
             
-            //if (ioctl(fd, TIOCGSERIAL, &serinfo) < 0)
-            //    return -1;
-            //if (ioctl(fd, TIOCSSERIAL, &serinfo) < 0)
-            //    return -1;
             tcgetattr(fd, &options);
             cfsetispeed(&options, rate_to_constant(baudRate));
             cfsetospeed(&options, rate_to_constant(baudRate));
-            //cfmakeraw(&options);
-            //options.c_cflag |= (CLOCAL | CREAD);
-            //options.c_cflag &= ~CRTSCTS;
             if (tcsetattr(fd, TCSANOW, &options) != 0) {
                 std::cerr << "Failed to set attr";
                 return;
             }
-            
-//            _serial.set_option(boost::asio::serial_port_base::baud_rate(baudRate));
+#endif
+#ifdef WINDOWS
+            _serial.set_option(boost::asio::serial_port_base::baud_rate(baudRate));
+#endif
             _serial.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
             _serial.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
             _serial.set_option(boost::asio::serial_port_base::character_size(8));
         }
 
-		boost::asio::serial_port& serialPort() { return _serial; }
+        boost::asio::serial_port& serialPort() { return _serial; }
 
         void write(QByteArray data)
         {
@@ -79,84 +77,84 @@ namespace Greis
         }
 
 
-		QByteArray read(qint64 maxlen)
-		{
-			QByteArray data(maxlen, 0);
-			int actualRead = read(data.data(), data.size());
-			data.resize(actualRead);
-			return data;
-		}
+        QByteArray read(qint64 maxlen)
+        {
+            QByteArray data(maxlen, 0);
+            int actualRead = read(data.data(), data.size());
+            data.resize(actualRead);
+            return data;
+        }
 
-		QByteArray peek(qint64 maxlen)
-		{
-			QByteArray buf(maxlen, 0);
-			int read = peek(buf.data(), maxlen);
-			buf.resize(read);
-			return buf;
-		}
-		
-		qint64 read(char * data, qint64 maxSize)
-		{
-			qint64 read = 0;
-			if (_peekedData.size() > maxSize)
-			{
-				int dataToRead = maxSize;
-				memcpy(data, _peekedData.data(), dataToRead);
-				int newBufLength = _peekedData.size() - dataToRead;
-				memmove(_peekedData.data(), _peekedData.data() + dataToRead, newBufLength);
-				_peekedData.resize(newBufLength);
-				return dataToRead;
-			} else if (_peekedData.size() > 0)
-			{
-				memcpy(data, _peekedData.data(), _peekedData.size());
-				read = _peekedData.size();
-				maxSize -= read;
-				data += read;
-				_peekedData.clear();
-			}
+        QByteArray peek(qint64 maxlen)
+        {
+            QByteArray buf(maxlen, 0);
+            int read = peek(buf.data(), maxlen);
+            buf.resize(read);
+            return buf;
+        }
+        
+        qint64 read(char * data, qint64 maxSize)
+        {
+            qint64 read = 0;
+            if (_peekedData.size() > maxSize)
+            {
+                int dataToRead = maxSize;
+                memcpy(data, _peekedData.data(), dataToRead);
+                int newBufLength = _peekedData.size() - dataToRead;
+                memmove(_peekedData.data(), _peekedData.data() + dataToRead, newBufLength);
+                _peekedData.resize(newBufLength);
+                return dataToRead;
+            } else if (_peekedData.size() > 0)
+            {
+                memcpy(data, _peekedData.data(), _peekedData.size());
+                read = _peekedData.size();
+                maxSize -= read;
+                data += read;
+                _peekedData.clear();
+            }
 
-			if (maxSize == 0)
-			{
-				return read;
-			}
+            if (maxSize == 0)
+            {
+                return read;
+            }
 
-			read += boost::asio::read(_serial, boost::asio::buffer(data, maxSize));
-			return read;
-		}
+            read += boost::asio::read(_serial, boost::asio::buffer(data, maxSize));
+            return read;
+        }
 
-		qint64 peek(char * data, qint64 maxSize)
-		{
-			if (_peekedData.size() >= maxSize)
-			{
-				memcpy(data, _peekedData.data(), maxSize);
-				return maxSize;
-			} else if (_peekedData.size() > 0)
-			{
-				memcpy(data, _peekedData.data(), _peekedData.size());
-				data += _peekedData.size();
-				maxSize -= _peekedData.size();
-				qint64 read = _peekedData.size();
-				QByteArray buf(maxSize, 0);
-				read += boost::asio::read(_serial, boost::asio::buffer(buf.data(), buf.size()));
-				memcpy(data, buf.data(), read - _peekedData.size());
-				_peekedData.append(buf);
-				return read;
-			}
+        qint64 peek(char * data, qint64 maxSize)
+        {
+            if (_peekedData.size() >= maxSize)
+            {
+                memcpy(data, _peekedData.data(), maxSize);
+                return maxSize;
+            } else if (_peekedData.size() > 0)
+            {
+                memcpy(data, _peekedData.data(), _peekedData.size());
+                data += _peekedData.size();
+                maxSize -= _peekedData.size();
+                qint64 read = _peekedData.size();
+                QByteArray buf(maxSize, 0);
+                read += boost::asio::read(_serial, boost::asio::buffer(buf.data(), buf.size()));
+                memcpy(data, buf.data(), read - _peekedData.size());
+                _peekedData.append(buf);
+                return read;
+            }
 
-			_peekedData.resize(maxSize);
-			qint64 read = boost::asio::read(_serial, boost::asio::buffer(_peekedData.data(), _peekedData.size()));
-			memcpy(data, _peekedData.data(), read);
-			return read;
-		}
+            _peekedData.resize(maxSize);
+            qint64 read = boost::asio::read(_serial, boost::asio::buffer(_peekedData.data(), _peekedData.size()));
+            memcpy(data, _peekedData.data(), read);
+            return read;
+        }
 
-		bool isOpen()
-		{
-			return _serial.is_open();
-		}
+        bool isOpen()
+        {
+            return _serial.is_open();
+        }
 
-		void close() 
-		{
-			_serial.close();
-		}
-	};
+        void close() 
+        {
+            _serial.close();
+        }
+    };
 }
