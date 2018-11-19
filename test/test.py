7bb10313c4ce06d73de02a02d4f0205320339d8a
Kirill Kholodkov <keir@ifz.ru>
2018-11-19 12:15:16 +0300

    testing fix

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import subprocess
 import json
 result = subprocess.run(['build/bin/testBed', 'test/ifz-data-0.jps'], stdout=subprocess.PIPE)
-epochs = json.loads(str(result.stdout))
+epochs = json.loads(result.stdout.decode("utf-8"))
 if len(epochs)==90:
     print("Test 1: pass")
