12e6528d8b3a1aaf2dafc5bb614119ec9a2d7edc
Kirill Kholodkov <keir@ifz.ru>
2018-11-19 11:53:08 +0300

    travis python version fix

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import subprocess
 import json
 result = subprocess.run(['build/bin/testBed', 'test/ifz-data-0.jps'], stdout=subprocess.PIPE)
-epochs = json.loads(result.stdout)
+epochs = json.loads(str(result.stdout))
 if len(epochs)==90:
     print("Test 1: pass")
